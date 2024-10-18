a9d71fea5a66a72a6ae8632f8da850ab38387a98
Maria Luiza Lima <132663650+mlrlima@users.noreply.github.com>
2024-10-18 14:30:21 -0300

    mudei db_creation.py

--- database_creation_files/db_creation.py
+++ database_creation_files/db_creation.py
@@ -2,68 +2,53 @@ import sqlite3
 
 conn = sqlite3.connect('FlavourIt.db')
 cursor = conn.cursor()
-cursor.execute("""
-CREATE TABLE Cliente (
-    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+cursor.execute('''CREATE TABLE IF NOT EXISTS Cliente (
+    Id TEXT PRIMARY KEY,
     Nome TEXT NOT NULL,
     Altura REAL,
     Peso REAL,
     Atividade TEXT
-);""")
-
-cursor.execute("""
-CREATE TABLE Favoritado (
-    Id INTEGER PRIMARY KEY AUTOINCREMENT,
-    Id_Receita INT,
-    Id_Cliente INT,
-    FOREIGN KEY (Id_Receita) REFERENCES Receita(Id),
-    FOREIGN KEY (Id_Cliente) REFERENCES Cliente(Id)
-);""")
-
-cursor.execute("""
-CREATE TABLE Ingrediente (
-    Id INTEGER PRIMARY KEY AUTOINCREMENT,
-    Id_Receita INT,
-    Id_Val_Nutri INT,
-    Quantidade REAL,
-    Unidade TEXT,
-    FOREIGN KEY (Id_Receita) REFERENCES Receita(Id),
-    FOREIGN KEY (Id_Val_Nutri) REFERENCES Valores_Nutricionais(Id)
-);""")
-
-cursor.execute("""
-CREATE TABLE Valores_Nutricionais (
-    Id INTEGER PRIMARY KEY AUTOINCREMENT,
-    Nome TEXT NOT NULL,
+)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS Valores_Nutricionais (
+    Id text primary key,
+    Nome TEXT,
     Gordura REAL,
     Carboidrato REAL,
     Proteina REAL,
     Porção REAL,
     Unidade TEXT
-);""")
-
-cursor.execute("""
-CREATE TABLE Receita_Utensilio (
-    Id INTEGER PRIMARY KEY AUTOINCREMENT,
-    Id_Receita INT,
-    Id_Utensilio INT,
+)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS Favoritado (
+    Id_Receita TEXT,
+    Id_val_nutri INT,
+    FOREIGN KEY(Id_Receita) REFERENCES Receita(Id),
+    FOREIGN KEY(Id_Val_Nutri) REFERENCES Valores_Nutricionais(Id)
+)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS Ingrediente (
+    Id TEXT PRIMARY KEY,
+    Id_Receita TEXT,
+    Id_Val_Nutri INT,
+    Quantidade REAL,
+    Unidade TEXT,
+    FOREIGN KEY(Id_Receita) REFERENCES Receita(Id),
+    FOREIGN KEY(Id_Val_Nutri) REFERENCES Valores_Nutricionais(Id)
+)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS Receita_Utensilio (
+    Id TEXT PRIMARY KEY,
+    Id_Receita TEXT,
+    Id_Utensilio TEXT,
     FOREIGN KEY (Id_Receita) REFERENCES Receita(Id),
     FOREIGN KEY (Id_Utensilio) REFERENCES Utensilio(Id)
-);""")
-
-cursor.execute("""
-CREATE TABLE Utensilio (
-    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS Utensilio (
+    Id TEXT PRIMARY KEY,
     Nome TEXT NOT NULL
-);""")
-
-cursor.execute("""
-CREATE TABLE Receita (
-    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+)''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS Receita (
+    Id TEXT PRIMARY KEY,
     Nome TEXT NOT NULL,
     Tempo TEXT NOT NULL,
     Instruções TEXT NOT NULL
-);""")
-
+)''')
 conn.commit()
 conn.close()
