f2dc02e51ed72cb53b5a38738107a45f48c34679
Gabriela Lemos <124183874+GabrielaRBLemos@users.noreply.github.com>
2024-10-19 21:55:41 -0300

    Barras de navegação para páginas com filtro

--- FlavourIt_app/views.py
+++ FlavourIt_app/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def menu(request):
     return render(request, 'flavourit/menu.html')
+
+def ingredient_filter(request):
+    return render(request, 'flavourit/ingredient_filter.html')
+
+def tool_filter(request):
+    return render(request, 'flavourit/tool_filter.html')
+
+def time_filter(request):
+    return render(request, 'flavourit/time_filter.html')
