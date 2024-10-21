836f655ed08e2016cfc6cf3cdb1ee04f78e08c97
Gabriela Lemos <124183874+GabrielaRBLemos@users.noreply.github.com>
2024-10-21 20:28:34 -0300

    Pesquisa por nome

--- FlavourIt/urls.py
+++ FlavourIt/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('tool_filter/',views.tool_filter,name='tool_filter'),
     path('time_filter/',views.time_filter,name='time_filter'),
     path('results/',views.recipe_results,name='recipe_results'),
+    path('search/',views.name_search,name='name_search'),
 ]
