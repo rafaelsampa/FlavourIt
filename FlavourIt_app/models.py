9d17ee3f614ea8e9f8db57c557d5b4ef9cb2d430
VictorHugoJSS <victor.-hugo@outlook.com.br>
2024-10-21 21:04:08 -0300

    models done

--- FlavourIt_app/models.py
+++ FlavourIt_app/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+class client(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    nome = models.TextField()
+    altura = models.DecimalField(max_digits=1, decimal_places=2)
+    peso = models.DecimalField(max_digits=3, decimal_places=2)
+    Atividade = models.TextField()
+
+class receita(models.Model):
+    id = models.BigAutoField()
+    nome = models.TextField()
+    tempo = models.TextField()
+    instructions = models.TextField()
+
+class valores_nutricionais(models.Model):
+    id = models.IntegerField()
+    nome = models.TextField()
+    gordura = models.DecimalField(max_digits=2, decimal_places=2)
+    carboidrato = models.DecimalField(max_digits=2, decimal_places=2)
+    proteina = models.DecimalField(max_digits=2, decimal_places=2)
+    porção = models.DecimalField(max_digits=2, decimal_places=2)
+    unidade = models.TextField()
+
+class utensilio(models.Model):
+    id = models.BigIntegerField()
+    nome = models.TextField()
+    
+class favoritado(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    id_Receita = models.ForeignKey(receita)
+    id_Cliente = models.ForeignKey(client)
+
+class ingredient(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    id_receita = models.ForeignKey(receita)
+    id_val_Nutri = models.ForeignKey(valores_nutricionais)
+    quant = models.DecimalField(max_digits=2, decimal_places=2, default=0)
+    unidade = models.TextField()
+
+class receita_utensilio(models.Model):
+    id = models.BigAutoField()
+    id_receita = models.ForeignKey(receita)
+    id_utensilio = models.ForeignKey(utensilio)
+
+
+
