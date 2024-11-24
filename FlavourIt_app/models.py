636e08f01531bc25fccb71419b98da3c62d78989
Roberto Regis <robertoregisaraujoneto@gmail.com>
2024-11-24 10:41:21 -0300

    Exibindo imagens dinamicamente (não commitei direito da última vez)

--- FlavourIt_app/models.py
+++ FlavourIt_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.templatetags.static import static
 
 class client(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -12,7 +13,16 @@ class receita(models.Model):
     nome = models.TextField()
     tempo = models.TextField()
     instructions = models.TextField()
-    
+
+    @property
+    def image_url(self):
+        file_name = f"{self.nome}.png"
+        image_path = f"graphics/img_recipes/{file_name}"
+        try:
+            return static(image_path)
+        except:
+            return static("graphics/default.jpg")
+
     def __str__(self):
         return self.nome
 
@@ -25,10 +35,24 @@ class valores_nutricionais(models.Model):
     porção = models.DecimalField(max_digits=2, decimal_places=2)
     unidade = models.TextField()
 
+    @property
+    def image_url(ingredient):
+        file_name = f"{ingredient.nome}.png"
+        image_path = f"graphics/img_ingredients/{file_name}"
+        return static(image_path)
+
+
+
 class utensilio(models.Model):
     id = models.BigAutoField(primary_key=True)
     nome = models.TextField()
-    
+
+    @property
+    def image_url(self):
+        file_name = f"{self.nome}.png"
+        image_path = f"graphics/img_utensils/{file_name}"
+        return static(image_path)
+
 class favoritado(models.Model):
     id = models.BigAutoField(primary_key=True)
     id_Receita = models.ForeignKey(receita, on_delete=models.CASCADE)
@@ -36,10 +60,14 @@ class favoritado(models.Model):
 
 class ingredient(models.Model):
     id = models.BigAutoField(primary_key=True)
-    id_receita = models.ForeignKey(receita, on_delete=models.CASCADE)
-    id_val_Nutri = models.ForeignKey(valores_nutricionais, on_delete=models.CASCADE)
+    nome = models.TextField()
+    id_receita = models.ForeignKey('receita', on_delete=models.CASCADE)
+    id_val_Nutri = models.ForeignKey('valores_nutricionais', on_delete=models.CASCADE)
     quant = models.DecimalField(max_digits=2, decimal_places=2, default=0)
     unidade = models.TextField()
+
+
+
 
 class receita_utensilio(models.Model):
     id = models.BigAutoField(primary_key=True)
