2fabd29cdab91dcd09d857c33003525d3385cb2b
Rafael S.S. <rafaelssampaiobr@gmail.com>
2024-11-28 18:00:43 -0300

    Create test_views.py

---
+++ FlavourIt_app/tests/test_views.py
@@ -0,0 +1,22 @@
+
+
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from decimal import Decimal
+from FlavourIt_app.models import (
+    receita, valores_nutricionais
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
